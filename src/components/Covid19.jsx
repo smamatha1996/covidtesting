079ff8642044849f93bf7c73b4155856c53e5266
Mamatha Reddy <mamathareddy.s123@gmail.com>
2022-07-22 13:59:58 +0530

    first commit

---
+++ src/components/Covid19.jsx
@@ -0,0 +1,34 @@
+import React,{useState, useEffect } from "react";
+import Loading from "./Loading";
+import CovidMap from "./CovidMap";
+import Legend from "./Legend";
+import LoadCountriesTask from "../tasks/LoadCountriesTask";
+
+const Covid19 = () => {
+  const [countries, setCountries] = useState([]);
+
+  const legendItemsReverse = [...legendItems].reverse();
+
+  const load = () => {
+    console.log("load");
+    const loadCountriesTask = new LoadCountriesTask();
+    loadCountriesTask.load((countries) => setCountries(countries));
+  };
+
+  useEffect(load, []);
+
+  return (
+    <div>
+      {countries.length === 0 ? (
+        <Loading />
+      ) : (
+        <div>
+          <CovidMap countries={countries} />
+          <Legend legendItems={legendItemsReverse} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Covid19;
